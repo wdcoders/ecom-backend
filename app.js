8559b492b83cf747c903c64d7e82d211d027ce11
wdcoders <59465038+wdcoders@users.noreply.github.com>
2023-05-03 00:53:02 +0530

    added port variable
    added port variable

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
+const PORT = 3000;
 
 app.get('/', (req, res) => {
 	res.json({ working: 'Hello App is working' });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 	console.log('Server started on port 3000');
 });
