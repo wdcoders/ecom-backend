92bf5e90b8f6d5009df04e8ad30e202e128258db
wdcoders <59465038+wdcoders@users.noreply.github.com>
2023-05-03 00:33:50 +0530

    Add Test route
    Add Test route

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.json({ working: 'Hello App is working' });
+});
+
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
+	console.log('Server started on port 3000');
 });
